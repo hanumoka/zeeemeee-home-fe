4d27d44c10f299fffab5b921e38db0727f577812
HANUYOGA\amagr <amagramer@gmail.com>
2024-10-03 18:45:04 +0900

    pico 적용 전

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const config = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
