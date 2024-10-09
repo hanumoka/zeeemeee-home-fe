abbb1ec06fdac15b3c7cfcdd2734dd4d61fc0f74
HANUYOGA\amagr <amagramer@gmail.com>
2024-10-09 14:39:28 +0900

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
       <h1>zeeemeee 페이지</h1>
+      <article>I’m a card!</article>
     </div>
   );
 }
